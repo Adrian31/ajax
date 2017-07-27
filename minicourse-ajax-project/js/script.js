55bf3659d80580f44b39e7b23e51b6b31e8241d7
Adrian31 <adrian_carabetta@hotmail.com>
2017-07-27 09:02:43 +0800

    Added streetview api, background image loading

--- minicourse-ajax-project/js/script.js
+++ minicourse-ajax-project/js/script.js
@@ -13,9 +13,20 @@ function loadData() {
 
     // load streetview
 
-    // YOUR CODE GOES HERE!
+    //https://maps.googleapis.com/maps/api/streetview?size=400x400&location=40.720032,-73.988354
+    //&fov=90&heading=235&pitch=10
+    //&key=AIzaSyBTu46MqI5xhnj3Upxwg5K7g4dhyuyqLO4
 
+    var streetStr = $('#street').val();
+    var cityStr = $('#city').val();
+    var address = streetStr + "," + cityStr;
+
+    var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
+    $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
+
+    // YOUR CODE GOES HERE!
     return false;
+    //600x400
 };
 
 $('#form-container').submit(loadData);
